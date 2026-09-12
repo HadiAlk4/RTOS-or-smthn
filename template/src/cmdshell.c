8af5559df48f83af97fe5450ba9f4fb88ddb5b29
HadiAlk4 <alkhubabdalhadi10@gmail.com>
2026-09-13 00:30:11 +1000

    (cmdshell): add strcmp for tasks and sd

--- template/src/cmdshell.c
+++ template/src/cmdshell.c
@@ -3,7 +3,7 @@
 // This is the command line interface (CLI)
 // You must have a Serial Terminal running (eg TeraTerm) to diaply and pass keypresses to this function
 // A readln() helper function is availble to read the keypresses into a buffer and return when teh Enter key is pressed.
-//		void readln(char *line, int size)  line is the buufer, size ensures that you do not overrun past the size of your buffer
+//		void readln(char *line, int size)  line is the buffer, size ensures that you do not overrun past the size of your buffer
 
 // add_Task wants that as:
 // add_Task(&flash, /*arg=*/ 3, "flash", /*priority=*/ 1);
@@ -15,7 +15,10 @@
 // "flash" | copied into TCB.name (what pt prints)
 // priority | stored in the TCB (RR ignores it until Part 2)
 
+void printTasks(void)
+{
 
+}
 
 void cmdShell(int dummy) {		// the dummy int is to keep consistent with the other tasks
 
@@ -23,7 +26,7 @@ void cmdShell(int dummy) {		// the dummy int is to keep consistent with the othe
 
     while(1)
     {
-        printf("🤙🐚 > "); // 📞
+        printf("🤙🐚 # "); // 📞
         readln(line, sizeof(line));
 
         char command[30];
@@ -31,5 +34,50 @@ void cmdShell(int dummy) {		// the dummy int is to keep consistent with the othe
         int n;
 
         n = sscanf(line, "%s %i %i", &command, &num1 ,&num2); // return number of successfully matched inputs
+
+        if(n < 1) continue;
+
+        if(strcmp(n == 1 && cmd, "pt") == 0)
+        {
+            printTasks();
+        }
+
+        else if(n == 2 && strcmp(cmd, "rt") == 0)
+        {
+            remove_Task(a);
+        }
+
+        else if(n == 1 && strcmp(cmd, "sd"))
+        {
+            for(int i=0;i<8;i++) display_buffer[i]=0;
+            printf("bye bye 😿\n");
+            DISABLE_INT(); // stop systick
+            while(1) {}
+        }
+
+        else if(n == 3 && strcmp(cmd, "blink"))
+        {
+            add_Task(&blink, a, "blink", b);
+        }
+
+        else if(n == 3 && strcmp(cmd, "flash"))
+        {
+            add_Task(&flash, a, "flash", b);
+        }
+
+        else if(n == 3 && strcmp(cmd, "hexer"))
+        {
+            add_Task(&hexer, a, "hexer", b);
+        }
+
+        else if(n == 3 && strcmp(cmd, "splat"))
+        {
+            add_Task(&splat, a, "splat", b);
+        }
+
+        else
+        {
+            printf("unknown cmd\n");
+        }
     }
 }

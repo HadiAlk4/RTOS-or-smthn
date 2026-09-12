d2de86019991ca05d65c7a544ef7216be2e4ac1c
HadiAlk4 <alkhubabdalhadi10@gmail.com>
2026-09-13 00:10:21 +1000

    (cmdshell): add readln

--- template/src/cmdshell.c
+++ template/src/cmdshell.c
@@ -5,6 +5,31 @@
 // A readln() helper function is availble to read the keypresses into a buffer and return when teh Enter key is pressed.
 //		void readln(char *line, int size)  line is the buufer, size ensures that you do not overrun past the size of your buffer
 
+// add_Task wants that as:
+// add_Task(&flash, /*arg=*/ 3, "flash", /*priority=*/ 1);
+// Look at the prototype:
+// extern int add_Task(void (*Task)(int), uint32_t arg, const char *name, uint8_t priority);
+// Parameter	Meaning
+// &flash | function to run (address of the task)
+// arg | goes into R0 → becomes pos / numflash
+// "flash" | copied into TCB.name (what pt prints)
+// priority | stored in the TCB (RR ignores it until Part 2)
+
+
+
 void cmdShell(int dummy) {		// the dummy int is to keep consistent with the other tasks
 
+    char line[30];
+
+    while(1)
+    {
+        printf("🤙🐚 > "); // 📞
+        readln(line, sizeof(line));
+
+        char command[30];
+        int num1, num2;
+        int n;
+
+        n = sscanf(line, "%s %i %i", &command, &num1 ,&num2); // return number of successfully matched inputs
+    }
 }
